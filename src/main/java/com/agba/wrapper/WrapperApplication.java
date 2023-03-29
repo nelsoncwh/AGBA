ef0c9541b8a4e4a3dd6551b7657cda69ca6ef40c
terryleung <terryleung@HK010201355.convoy.local>
2023-03-29 12:29:34 +0800

    add component scan

--- src/main/java/com/agba/wrapper/WrapperApplication.java
+++ src/main/java/com/agba/wrapper/WrapperApplication.java
@@ -4,10 +4,12 @@ import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 @OpenAPIDefinition(info = @Info(title = "APIs", version = "1.0",
     description = "Documentation APIs v1.0"))
+@ComponentScan({"com.agba.wrapper","com.agba.wealth"})
 public class WrapperApplication {
 
   public static void main(String[] args) {
